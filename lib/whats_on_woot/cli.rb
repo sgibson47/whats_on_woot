bb25e295c5d088ed1273b5ec97e323f84644a299
sgibson47 <sgibson47@gmail.com>
2018-06-05 13:26:20 -0500

    request user input and respond

--- lib/whats_on_woot/cli.rb
+++ lib/whats_on_woot/cli.rb
@@ -17,6 +17,17 @@ class WhatsOnWoot::CLI
   def menu
     puts "Enter a number from the list to get more information"
     puts "about the corresponding deal, or exit to quit."
+    input = gets.strip
+
+    if input == "1"
+      puts "more info on deal 1"
+    elsif input == "2"
+      puts "more info on deal 2"
+    elsif input == "3"
+      puts "more info on deal 3"
+    else
+      puts "Please enter a number from the list or exit."
+    end
   end
 
 end

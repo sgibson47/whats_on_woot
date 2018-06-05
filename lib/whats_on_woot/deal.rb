8ff333bdbf1b26139f05a999005a469cc7dfbb74
sgibson47 <sgibson47@gmail.com>
2018-06-05 14:05:24 -0500

    added url attribute to deal class & exposed same in display_details

--- lib/whats_on_woot/deal.rb
+++ lib/whats_on_woot/deal.rb
@@ -1,5 +1,5 @@
 class WhatsOnWoot::Deal
-  attr_accessor :product, :price
+  attr_accessor :product, :price, :url
 
   @@all =[]
 
@@ -11,21 +11,26 @@ class WhatsOnWoot::Deal
     puts "\n"
     puts "#{self.product}"
     puts "is on sale for $#{self.price}."
+    puts "Check it out here:"
+    puts "#{self.url}"
   end 
 
   deal_1 = self.new
   deal_1.product = "a fancy computer"
   deal_1.price = "$$$$"
+  deal_1.url = "url1"
   @@all << deal_1
 
   deal_2 = self.new
   deal_2.product = "a fancy camera"
   deal_2.price = "$$"
+  deal_2.url = "url2"
   @@all << deal_2
 
   deal_3 = self.new
   deal_3.product = "a t-shirt"
   deal_3.price = "$"
+  deal_3.url = "url3"
   @@all << deal_3
 
 end

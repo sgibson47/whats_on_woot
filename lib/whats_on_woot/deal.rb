ebdd34748f8ae3453dc46842b172ee9e9d0850ec
sgibson47 <sgibson47@gmail.com>
2018-06-05 15:27:54 -0500

    formatted #display_details

--- lib/whats_on_woot/deal.rb
+++ lib/whats_on_woot/deal.rb
@@ -10,7 +10,7 @@ class WhatsOnWoot::Deal
   def display_details
     puts "\n"
     puts "#{self.product}"
-    puts "is on sale for $#{self.price}."
+    puts "is on sale for #{self.price}."
     puts "Check it out here:"
     puts "#{self.url}"
   end 
@@ -22,23 +22,5 @@ class WhatsOnWoot::Deal
     end
     @@all<<thing
   end
-
-  deal_1 = self.new
-  deal_1.product = "a fancy computer"
-  deal_1.price = "$$$$"
-  deal_1.url = "url1"
-  @@all << deal_1
-
-  deal_2 = self.new
-  deal_2.product = "a fancy camera"
-  deal_2.price = "$$"
-  deal_2.url = "url2"
-  @@all << deal_2
-
-  deal_3 = self.new
-  deal_3.product = "a t-shirt"
-  deal_3.price = "$"
-  deal_3.url = "url3"
-  @@all << deal_3
 
 end

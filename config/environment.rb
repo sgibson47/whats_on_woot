10206ab0ea063552e2a58bda0a81d98d094c8ab5
sgibson47 <sgibson47@gmail.com>
2018-06-05 14:38:04 -0500

    identify css selectors for desired data and set up to put data into a hash

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,4 @@ require 'pry'
 require_relative "../lib/whats_on_woot/version"
 require_relative "../lib/whats_on_woot/cli"
 require_relative "../lib/whats_on_woot/deal"
+require_relative "../lib/whats_on_woot/scraper"
--- lib/whats_on_woot/scraper.rb
+++ lib/whats_on_woot/scraper.rb
@@ -1,13 +1,18 @@
 class WhatsOnWoot::Scraper
 
   def get_page
-    Nokogiri::HTML(open("https://www.woot.com/"))
+    doc = Nokogiri::HTML(open("https://www.woot.com/"))
+
+    binding.pry 
   end
 
   def scrape_page
     #deal_hash ={}
     
-    #deal_hash[:product] = self.get_page.css("")
+    #deal_hash[:product] = self.get_page.css("h2.main-title.fn").text
+    #deal_hash[:price] = self.get_page.css("span.price").text
+    #deal_hash[:url] = "https://www.woot.com#{self.get_page.css("p.read-more a.url").attribute("href").value}"
+    #deal_hash
   end
 
 end

a4dce474655398622624dca44fbdb39ce5c30975
sgibson47 <sgibson47@gmail.com>
2018-06-05 15:11:33 -0500

    added #scrape tools

--- lib/whats_on_woot/scraper.rb
+++ lib/whats_on_woot/scraper.rb
@@ -59,4 +59,15 @@ class WhatsOnWoot::Scraper
     deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
     @deals << deal_hash
   end
+
+  def scrape_tools
+    doc = get_page("https://www.woot.com/category/tools")
+    
+    deal_hash ={}
+    
+    deal_hash[:product] = doc.css("h2.main-title.fn").text
+    deal_hash[:price] = doc.css("span.price").text
+    deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
+    @deals << deal_hash
+  end
 end

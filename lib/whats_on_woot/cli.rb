f206842842d5dc06f49e170761fbf73101ab131c
sgibson47 <sgibson47@gmail.com>
2018-06-05 13:10:34 -0500

    broke #call down into 2 methods - #list deals & #menu

--- lib/whats_on_woot/cli.rb
+++ lib/whats_on_woot/cli.rb
@@ -1,12 +1,20 @@
 class WhatsOnWoot::CLI
   
   def call
+    list_deals
+    menu
+  end
+
+  def list_deals
     puts "What's on Woot?"
     puts "\n"
     puts "1. Deal 1"
     puts "2. Deal 2"
     puts "3. Deal 3"
     puts "\n"
+  end
+
+  def menu
     puts "Enter a number from the list to get more information"
     puts "about the corresponding deal, or exit to quit."
   end

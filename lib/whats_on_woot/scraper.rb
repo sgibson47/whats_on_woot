b11125c68f0b5ddfa9457200fef011f74a97a5b2
sgibson47 <sgibson47@gmail.com>
2018-06-05 15:47:45 -0500

    refactor, eliminated various #scrape_[notwoot]in favor of #scrape_wootish

--- lib/whats_on_woot/scraper.rb
+++ lib/whats_on_woot/scraper.rb
@@ -11,12 +11,7 @@ class WhatsOnWoot::Scraper
 
   def scrape_pages
     scrape_woot
-    scrape_home
-    scrape_electronics
-    scrape_computers
-    scrape_tools
-    scrape_sport
-    scrape_wine
+    scrape_wootish
   end 
 
   def make_deals
@@ -37,69 +32,17 @@ class WhatsOnWoot::Scraper
     @deals << deal_hash
   end
 
-  def scrape_home
-    doc = get_page("https://www.woot.com/category/home")
+  def scrape_wootish
+    categories = ["home", "electronics","computers", "tools", "sport", "wine"]
     
-    deal_hash ={}
-    
-    deal_hash[:product] = doc.css("h2.main-title.fn").text
-    deal_hash[:price] = doc.css("span.price").text
-    deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
-    @deals << deal_hash
-  end 
-
-  def scrape_electronics
-    doc = get_page("https://www.woot.com/category/electronics")
-    
-    deal_hash ={}
-    
-    deal_hash[:product] = doc.css("h2.main-title.fn").text
-    deal_hash[:price] = doc.css("span.price").text
-    deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
-    @deals << deal_hash
+    categories.each do |category|
+      doc = get_page("https://www.woot.com/category/#{category}")
+      deal_hash ={}
+      deal_hash[:product] = doc.css("h2.main-title.fn").text
+      deal_hash[:price] = doc.css("span.price").text
+      deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
+      @deals << deal_hash
+    end
   end
 
-  def scrape_computers
-    doc = get_page("https://www.woot.com/category/computers")
-    
-    deal_hash ={}
-    
-    deal_hash[:product] = doc.css("h2.main-title.fn").text
-    deal_hash[:price] = doc.css("span.price").text
-    deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
-    @deals << deal_hash
-  end
-
-  def scrape_tools
-    doc = get_page("https://www.woot.com/category/tools")
-    
-    deal_hash ={}
-    
-    deal_hash[:product] = doc.css("h2.main-title.fn").text
-    deal_hash[:price] = doc.css("span.price").text
-    deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
-    @deals << deal_hash
-  end
-
-  def scrape_sport
-    doc = get_page("https://www.woot.com/category/sport")
-    
-    deal_hash ={}
-    
-    deal_hash[:product] = doc.css("h2.main-title.fn").text
-    deal_hash[:price] = doc.css("span.price").text
-    deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
-    @deals << deal_hash
-  end
-
-  def scrape_wine
-    doc = get_page("https://www.woot.com/category/wine")
-    
-    deal_hash ={}
-    
-    deal_hash[:product] = doc.css("h2.main-title.fn").text
-    deal_hash[:price] = doc.css("span.price").text
-    deal_hash[:url] = "#{doc.css("a.woot-flat-button").attribute("href").value}"
-    @deals << deal_hash
-  end
 end

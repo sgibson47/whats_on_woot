f3b26336998067b441682e20174a0274864757a5
sgibson47 <sgibson47@gmail.com>
2018-06-05 13:37:27 -0500

    added class constant @@all, set it equal to an empty array, and pushed deals 1, 2, & 3 into it

--- lib/whats_on_woot/deal.rb
+++ lib/whats_on_woot/deal.rb
@@ -1,16 +1,21 @@
 class WhatsOnWoot::Deal
   attr_accessor :product, :price
 
+  @@all =[]
+
   deal_1 = self.new
   deal_1.product = "a fancy computer"
   deal_1.price = "$$$$"
+  @@all << deal_1
 
   deal_2 = self.new
   deal_2.product = "a fancy camera"
   deal_2.price = "$$"
+  @@all << deal_2
 
   deal_3 = self.new
   deal_3.product = "a t-shirt"
   deal_3.price = "$"
+  @@all << deal_3
 
 end
